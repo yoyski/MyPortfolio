7a10ffc83a1c225376837f47be4c0ff4bdacf5e6
yoyski <ferdinandasjr.@gmauil.com>
2025-07-09 09:04:19 +0800

    ok

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,7 @@ function App() {
     <>
       <Header />
       <Routes>
-        <Route path="/MyPortfolio" element={<Home />} />
+        <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/projects" element={<Projects />} />
         <Route path="/contact" element={<Contact />} />
